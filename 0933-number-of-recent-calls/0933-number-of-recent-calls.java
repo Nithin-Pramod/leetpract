908d7fd72419e80e0f5b6e83f56cf6213d1ebf43
Nithin N P <43591076+Nithin-Pramod@users.noreply.github.com>
2024-06-26 18:57:34 +0530

    Time: 20 ms (90.38%), Space: 54 MB (66.96%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -1,24 +1,29 @@
 class RecentCounter {
-    
+
+    // Queue to store the timestamps of the pings
     Queue<Integer> queue;
 
+    // Constructor initializes the queue
     public RecentCounter() {
         queue = new LinkedList<>();
     }
-    
+
+    /**
+     * Records a ping at time t.
+     * @param t The current timestamp (in milliseconds).
+     * @return The number of pings that have been made in the past 3000 milliseconds (including the current ping).
+     */
     public int ping(int t) {
         
+        // Add the current ping timestamp to the queue
         queue.add(t);
         
-        while(queue.peek() < t-3000) queue.poll();
+        // Remove pings from the queue that are older than 3000 milliseconds
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
         
+        // Return the number of pings in the past 3000 milliseconds
         return queue.size();
-        
     }
 }
-
-/**
- * Your RecentCounter object will be instantiated and called as such:
- * RecentCounter obj = new RecentCounter();
- * int param_1 = obj.ping(t);
- */
