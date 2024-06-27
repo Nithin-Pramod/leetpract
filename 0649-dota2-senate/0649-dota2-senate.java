a4393ba5f99e021791408af05fb4877c702436c5
Nithin N P <43591076+Nithin-Pramod@users.noreply.github.com>
2024-06-27 11:39:41 +0530

    Time: 9 ms (87.65%), Space: 45.4 MB (46.85%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -1,29 +1,33 @@
 class Solution {
     public String predictPartyVictory(String senate) {
-        
+        // Convert the senate string to a character array for easier manipulation
         char[] senateArr = senate.toCharArray();
         
         int length = senateArr.length;
         
-        
+        // Initialize two queues to store the indices of 'R' (Radiant) and 'D' (Dire) senators
         Queue<Integer> rQueue = new LinkedList<>();
         Queue<Integer> dQueue = new LinkedList<>();
         
-        for(int i =0 ; i < length ; i ++){
-            if(senateArr[i]=='R') rQueue.add(i);
-            else  dQueue.add(i);
+        // Populate the queues with the initial indices of Radiant and Dire senators
+        for (int i = 0; i < length; i++) {
+            if (senateArr[i] == 'R') rQueue.add(i);
+            else dQueue.add(i);
         }
         
-        while(dQueue.size()!=0 && rQueue.size()!=0 ){
+        // Process the queues until one of them is empty
+        while (dQueue.size() != 0 && rQueue.size() != 0) {
+            // Get the indices of the current senators from both queues
             int dTurn = dQueue.poll();
             int rTurn = rQueue.poll();
             
-            if(rTurn < dTurn) rQueue.add(rTurn+length);
-            else dQueue.add(dTurn+length);
+            // The senator with the smaller index gets to ban the other
+            // The winner's index is increased by the length of the senate array and added back to the queue
+            if (rTurn < dTurn) rQueue.add(rTurn + length);
+            else dQueue.add(dTurn + length);
         }
         
-        return rQueue.size()>dQueue.size() ? "Radiant":"Dire";
-        
-        
+        // Determine the winning party based on the remaining senators in the queues
+        return rQueue.size() > dQueue.size() ? "Radiant" : "Dire";
     }
 }
